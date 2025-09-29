17e62c4685e261821ebf9dae946918eab391cfaf
DiegoCarlito <diego.carlito01@gmail.com>
2025-09-29 00:56:04 -0300

    feat(products): implementa endpoint de listagem de produtos

--- src/core/urls.py
+++ src/core/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import HelloWorldView
+from products.views import HelloWorldView, ProductListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/hello/', HelloWorldView.as_view(), name='hello_world'),
+    path('api/products/', ProductListView.as_view(), name='product-list'),
 ]
--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    CODIGO_BARRAS_MAX_LENGTH = 13
+    NOME_MAX_LENGTH = 100
+
+    codigo_barras = models.CharField(max_length=CODIGO_BARRAS_MAX_LENGTH, unique=True, help_text="Código de barras")
+    nome = models.CharField(max_length=NOME_MAX_LENGTH)
+    descricao = models.TextField(blank=True, null=True)
+
+    TIPO_PRODUTO_CHOICES = [
+        ('UNITARIO', 'Produto Unitário'),
+        ('PESAVEL', 'Produto Pesável'),
+    ]
+    tipo = models.CharField(max_length=10, choices=TIPO_PRODUTO_CHOICES, default='UNITARIO')
+
+    ativo = models.BooleanField(default=True, help_text="Indica se o produto está ativo no catálogo")
+
+    def __str__(self):
+        return f"{self.nome} ({self.codigo_barras})"
