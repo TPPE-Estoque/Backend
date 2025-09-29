f59c365a3810f88296b8d5b138cb6058a3a4f7f9
DiegoCarlito <diego.carlito01@gmail.com>
2025-09-28 23:39:13 -0300

    feat(api): adiciona endpoint de teste 'hello world'

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HelloWorldView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {"message": "Hello World!"}
+        return Response(data, status=status.HTTP_200_OK)
