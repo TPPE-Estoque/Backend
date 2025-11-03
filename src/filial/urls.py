db7062abee5587e5abe33015183670055e9849a2
DiegoCarlito <diego.carlito01@gmail.com>
2025-11-03 09:54:28 -0300

    (#13) test(filial): implementa testes de integração para CRUD de Filial

--- src/filial/urls.py
+++ src/filial/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import FilialListCreateView, FilialDetailView
 
+app_name = 'filial'
+
 urlpatterns = [
     path('', FilialListCreateView.as_view(), name='filial-lista-criar'),
     path('<int:pk>/', FilialDetailView.as_view(), name='filial-detalhe'),
